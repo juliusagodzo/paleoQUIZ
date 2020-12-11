f8e228f19be5d6694ceeb31c12fdb442ad9b2d80
lory2k01 <tecnolory01@gmail.com>
2020-12-11 11:39:58 +0100

    Initial commit

---
+++ js/questions.js
@@ -0,0 +1,105 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Si deve porre diverso da zero per trovare il dominio di una funzione razionale fratta",
+    answer: "Denominatore",
+    options: [
+      "Denominatore",
+      "Frazione",
+      "Funzione",
+      "Numeratore"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Qual è il punto in cui cadono infiniti elementi?",
+    answer: "Punto di accumulazione",
+    options: [
+      "Insieme infinito",
+      "Punto isolato",
+      "Punto di accumulazione",
+      "Intervallo infinto"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Una circoferenza rappresenta una funzione",
+    answer: "Non rappresenta alcuna funzione",
+    options: [
+      "Non rappresenta alcuna funzione",
+      "Suriettiva",
+      "Iniettiva",
+      "Sia suriettiva che iniettiva"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Una retta obliqua rappresenta una funzione dispari",
+    answer: "Mai",
+    options: [
+      "Sempre",
+      "Solo se è crescente",
+      "Mai",
+      "Solo se è decrescente"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Le funzioni possono essere sia crescenti e decrescenti",
+    answer: "Vero",
+    options: [
+      "Vero",
+      "Falso",
+      "e pari",
+      "e pari o dispari"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Il CUBO di X può essere",
+    answer: "Dispari e crescente",
+    options: [
+      "Pari e crescente",
+      "Crescente o pari",
+      "Dispari e decrescente",
+      "Dispari e crescente"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Cos'è il dominio di una funzione?",
+    answer: "L'insieme dei valori x per cui è possibile determinare Y",
+    options: [
+      "L'insieme dei valori x per cui è possibile determinare Y",
+      "L'insieme dei valori y per cui è possibile determinare X",
+      "L'immagine della funzione stessa",
+      "Il quadrante contenente la curva della funzione"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Cos'è il dominio di una funzione?",
+    answer: "Tutti i valori che può assumere la variabile indipendente",
+    options: [
+      "I valori assunti dalla funzione",
+      "I valori che si possono assegnare alla variabile y",
+      "Tutti i valori che può assumere la variabile indipendente",
+      "L'insieme dei valori x per cui non è possibile determinare Y",
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
